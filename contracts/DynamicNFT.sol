bd2c64c8eb6abfa2f61ab19dbda376cdff46b5de
erfanzar <erfanzare82@yahoo.com>
2022-11-15 18:12:56 +0330

    bugs fix

--- contracts/DynamicNFT.sol
+++ contracts/DynamicNFT.sol
@@ -2,42 +2,81 @@ pragma solidity ^0.6.6;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@chainlink/contracts/src/v0.6/VRFConsumerBase.sol";
-
 import "@openzeppelin/contracts/access/Ownable.sol";
-// import "@openzeppelin/contracts/utils/Strings.sol";
-// tks = 0x326C977E6efc84E512bB9C30f76E30c160eD06FB
 
-contract DynamicNFT is ERC721 {
+contract DynamicNFT is ERC721, VRFConsumerBase, Ownable {
+    using SafeMath for uint256;
+    using Strings for string;
+
     bytes32 internal keyHash;
     uint256 internal fee;
     uint256 public randomResult;
     address public VRFCoordinator;
+    // rinkeby: 0xb3dCcb4Cf7a26f6cf6B120Cf5A73875B7BBc655B
+    address public LinkToken;
+    // rinkeby: 0x01BE23585060835E02B77ef475b0Cc51aA1e0709a
+
+    struct Character {
+        string name;
+    }
+
+    Character[] public characters;
+
+    mapping(bytes32 => string) requestToCharacterName;
+    mapping(bytes32 => address) requestToSender;
+    mapping(bytes32 => uint256) requestToTokenId;
 
     constructor(
-        // address _VRFCoordinator,
-        // address _LinkToken,
+        address _VRFCoordinator,
+        address _LinkToken,
         bytes32 _keyhash
     )
         public
-        
+        VRFConsumerBase(_VRFCoordinator, _LinkToken)
         ERC721("DynamicNFT", "DNFT")
     {
-        // VRFCoordinator = _VRFCoordinator;
+        VRFCoordinator = _VRFCoordinator;
+        LinkToken = _LinkToken;
         keyHash = _keyhash;
-        fee = 0.1*10**18;
+        fee = 0.1 * 10**18; // 0.1 LINK
     }
 
-    struct lvl{
-        uint256 level;
+    function requestNewRandomCharacter(string memory name)
+        public
+        returns (bytes32)
+    {
+        require(
+            LINK.balanceOf(address(this)) >= fee,
+            "Not enough LINK - fill contract with faucet"
+        );
+        bytes32 requestId = requestRandomness(keyHash, fee);
+        requestToCharacterName[requestId] = name;
+        requestToSender[requestId] = msg.sender;
+        return requestId;
     }
 
-    lvl[] public NFTLVL;
+    function getTokenURI(uint256 tokenId) public view returns (string memory) {
+        return tokenURI(tokenId);
+    }
 
-    function setTokenURI(uint256 tokenId,string memory _tokenURI) public{
+    function setTokenURI(uint256 tokenId, string memory _tokenURI) public {
         require(
             _isApprovedOrOwner(_msgSender(), tokenId),
             "ERC721: transfer caller is not owner nor approved"
         );
         _setTokenURI(tokenId, _tokenURI);
     }
+
+    function fulfillRandomness(bytes32 requestId, uint256 randomNumber)
+        internal
+        override
+    {
+        uint256 newId = characters.length;
+       
+        _safeMint(requestToSender[requestId], newId);
+    }
+
+
+
+
 }
--- migrations/2_DynamicNFT.js
+++ migrations/2_DynamicNFT.js
@@ -4,5 +4,5 @@ const VRF_COORDINATOR = '0x7a1BaC17Ccc5b313516C5E16fb24f7659aA5ebed'
 const KEY_HASH = '0x4b09e658ed251bcafeebbc69400383d49f344ace09b9576fe248bb02c003fe9f'
 
 module.exports = async (deployer, network, [defaultAccount]) => {
-    deployer.deploy(DN,KEY_HASH)
+    deployer.deploy(DN,VRF_COORDINATOR,LINK_TOKEN,KEY_HASH)
 }
